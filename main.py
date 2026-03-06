fde0f678f4cd611660eeb6fa65fc8a71175709e0
jukkas <jukkas@pm.me>
2026-03-06 20:26:50 +0200

    added separate health endpoint

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from src.api.message_api import router as api_router
 
 app = FastAPI()
 
+@app.get("/health", include_in_schema=False)
+async def health():
+    return {"status": "ok"}
+
 # Include the API router
 app.include_router(api_router)
